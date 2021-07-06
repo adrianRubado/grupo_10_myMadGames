1e72801538f07f6d6ceb7ad8956e959fd6916567
Adrian Ariel Rubado <aarubado@baufest.com>
2021-07-06 19:28:48 -0300

    delete, casi post

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,13 +1,29 @@
 const express = require ('express') ;
 const router = express.Router () ;
+const path = require('path')
+const multer = require('multer')
 const productsController = require ('../controllers/productsController') ;
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'../../public/images'))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = file.originalname + Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null,uniqueSuffix)
+    }
+  })
+
+ /*  var upload = multer({ storage})
+  ,upload.single('image')
+ */
 
 router.get('/',productsController.get)
 router.get ('/create', productsController.create)
 router.get('/:id/',productsController.detail )
 router.get('/:id/edit',productsController.edit)
 router.put('/:id/edit',productsController.update)
-router.post('/create', productsController.post)
+router.post('/', productsController.post)
 router.delete('/:id/delete',productsController.delete)
 
 module.exports = router
