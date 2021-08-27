c2d5efe499cf9a400238c65aafc07541e9eb7e22
Adrian Ariel Rubado <aarubado@baufest.com>
2021-08-27 12:04:59 -0300

    signUpController sequelize

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,14 +1,8 @@
 const fs= require('fs') ;
 const path = require ('path');
 const {check,validationResult} = require('express-validator');
-<<<<<<< HEAD
-const userFilePath = path.join(__dirname, '../database/users.json');
-const userr = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+const db = require('../../database/models')
 const bcrypt= require ('bcryptjs');
-=======
-const bcrypt= require ('bcryptjs') ;
-const db = require ('../../database/models');
->>>>>>> d3ddae703d318f44783d4f557e304a8c3c4ad1b3
 
 
 
@@ -23,7 +17,7 @@ const signUpController = {
         res.render('sign-up');
     },
 
-    createUser: async(req,res) => {
+    createUser: (req,res) => {
 
          const errors = validationResult(req)
          if (!errors.isEmpty()){
@@ -31,45 +25,34 @@ const signUpController = {
             return res.status(400).json({errors:errors.array()}) ;
          }
 
-
-
-
-
-         const email= req.body.email;
          const user = db.User.findAll({
-             limit:1,
-             where:{
-                 email:req.body.email
-             }
-         })
-        
+            limit: 1,
+            where: {
+                email: req.body.email
+            }
+
+        })
 
          if(user){
             res.status(400).json({errors : {message : 'User already exists'}})
          }
+         const password = bcrypt.hashSync(req.body.password,10) ;
 
-         const newpassword = bcrypt.hashSync(req.body.password,10) ;
-       
-
-          await db.User.create ({
-            first_name:req.body.first_name,
-            last_name:req.body.last_name,
-            password:newpassword,
-            email:req.body.email,
-            bday:req.body.bday,
-            image:"/images/" + req.file.originalname,
+        await db.User.create({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            password: password,
+            email: req.body.email,
+            bday: req.body.bday,
+            image: "/images/" + req.file.originalname,
             role_id: 1
-         })
-        
+
+        })
+
       return res.redirect('/user/login/')
 
 
-    
+    }
 }
-
-
-
-}
-
 
 module.exports = signUpController
