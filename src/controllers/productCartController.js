3cd66143c8107cd59aafd0c6b3cb3a41d30cd2bd
Adrian Ariel Rubado <aarubado@baufest.com>
2021-08-31 17:31:35 -0300

    carrito sequelize

--- src/controllers/productCartController.js
+++ src/controllers/productCartController.js
@@ -11,13 +11,33 @@ const db = require('../../database/models')
     addItem : async (req,res) =>{
         const itemToAdd = req.body.add.split(',')
 
+        const alreadyExists = await db.Cart.findOne({
+            where: {
+                UserId: parseInt(itemToAdd[0]),
+                GameId : parseInt(itemToAdd[1])
+            }
+
+        })
+
+        if(alreadyExists){
+            await db.Cart.update({
+                quantity: alreadyExists.quantity + parseInt(itemToAdd[2]),
+                price: alreadyExists.price + (parseInt(itemToAdd[3]) * parseInt(itemToAdd[2]))
+
+            },{
+                where:{
+                    id:alreadyExists.id
+                }})
+        }else{
+
         await db.Cart.create({
             UserId: parseInt(itemToAdd[0]),
             GameId: parseInt(itemToAdd[1]),
             quantity: parseInt(itemToAdd[2]),
-            price: parseInt(itemToAdd[3])
+            price: parseInt(itemToAdd[3] * parseInt(itemToAdd[2]))
 
         })
+    }
 
 
     }
