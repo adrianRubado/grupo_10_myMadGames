63f5b49918a904b89d736a3582e4b4a43abee58b
Adrian Ariel Rubado <aarubado@baufest.com>
2021-09-05 21:18:05 -0300

    validaciones back-end

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -26,17 +26,28 @@ router.get ('/sign-up',guestMiddleware, signUpController.signUp) ;
 
 router.post ('/sign-up',upload.single('image'),[
   check('firstName').not().isEmpty().withMessage ('Debes completar el Nombre') ,
+  check('firstName').isLength({min :2}).withMessage ('El nombre debe contener minimo 2 caracteres'),
+
   check('lastName').not().isEmpty().withMessage ('Debes completar el apellido'),
-  check('password').isLength({min:5}).withMessage('La contraseña debe contar con mas de 8 caracteres'),
-  check('email').isEmail()
+  check('lastName').isLength({min :2}).withMessage ('El apellido debe contener minimo 2 caracteres'),
+
+  check('password').not().isEmpty().withMessage ('Debes completar la password') ,
+  check("password").matches(/^(?=.\d)(?=.[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i"),
+
+  check('email').not().isEmpty().withMessage ('Debes completar el email'),
+  check('email').isEmail(),
+
+
 
 ], signUpController.createUser);
 
 
 router.get ('/login',guestMiddleware, signInOutController.signIn) ;
 router.post ('/login', [
-
+    check('email').not().isEmpty().withMessage ('Debes completar el email'),
     check('email', 'Please include a valid email').isEmail(),
+
+
     check('password', 'The password is required').exists()
 
 
