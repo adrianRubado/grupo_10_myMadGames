73f0df8d10cd5721087407f10f137f785e11dbcc
Adrian Ariel Rubado <aarubado@baufest.com>
2021-09-10 13:25:17 -0300

    Fix height signUp form

--- public/validations/signup-scripts.js
+++ public/validations/signup-scripts.js
@@ -4,25 +4,13 @@ window.addEventListener("load",function(){
 
 //Primero capturamos el formulario a trabajar
 
-<<<<<<< HEAD:public/validations/signup-scripts.js
 let formsSignup = document.querySelector(".formularioSignUp");
 
 
-=======
-let formsSignup =document.querySelector('.formularioSignUp');
->>>>>>> bruno-dev:src/validations/signup-scripts.js
 
 
 //Definimos un array con los errores a reflejar
 
-
-
-<<<<<<< HEAD:public/validations/signup-scripts.js
-=======
-formsSignup.addEventListener('submit',  function(e){
-    e.preventDefault() ;
-    
->>>>>>> bruno-dev:src/validations/signup-scripts.js
 
 formsSignup.addEventListener("submit", function(e){
 
@@ -34,38 +22,26 @@ formsSignup.addEventListener("submit", function(e){
     const email = document.querySelector ('#email');
     /* fetch ('http://localhost:3002/sign-up/check') ; */
     const birthDay = document.querySelector ('#bday');
-<<<<<<< HEAD:public/validations/signup-scripts.js
-    const fileImage = document.querySelector('#botonCoverPhoto');
-                        //Definimos el acceso a cada uno de los campos de los formularios
-
-//Nombre
-    if(firstName.value ===''){
-        errors.push ('El Nombre está vacío');
-    }
-    if((firstName.value.length < 2)){
-
-        errors.push ('El Nombre contiene menos de dos caracteres') ;
-=======
     const fileImage = document.querySelector('#botonCoverPhoto');                           //Definimos el acceso a cada uno de los campos de los formularios
     const small = document.querySelector('small');
 //Nombre
 
 
-function setErrorsFor(input,msg){ 
+function setErrorsFor(input,msg){
     const formular=input.parentElement;
     const small =formular.querySelector('small');
     small.innerText = msg;
     formular.className= 'formularySignup error';
 
-    
+
 }
-function setSuccesFor(input){ 
+function setSuccesFor(input){
     const formular=input.parentElement;
     const small =formular.querySelector('small');
-   
+
     formular.className= 'formularySignup succes';
 
-    
+
 }
 
     if(firstName.value.trim() ===''){
@@ -75,22 +51,13 @@ function setSuccesFor(input){
     }
     if((firstName.value.length < 2)){
 
-       setErrorsFor (firstName,'El Nombre contiene menos de dos caracteres')                                
->>>>>>> bruno-dev:src/validations/signup-scripts.js
+       setErrorsFor (firstName,'El Nombre contiene menos de dos caracteres')
 
     }else{
         setSuccesFor(firstName)
     }
 
 //Apellido
-<<<<<<< HEAD:public/validations/signup-scripts.js
-    if(lastName.value ===''){
-        errors.push ('El Apellido está vacío');
-    }
-    if((lastName.value.length < 2)){
-
-        errors.push ('El Apellido contiene menos de dos caracteres') ;
-=======
     if(lastName.value.trim ==''){
         setErrorsFor (lastName,'El Apellido está vacío');
     }else{
@@ -98,8 +65,7 @@ function setSuccesFor(input){
     }
     if((lastName.value.length < 2)){
 
-        setErrorsFor (lastName,'El Apellido contiene menos de dos caracteres') ;                                
->>>>>>> bruno-dev:src/validations/signup-scripts.js
+        setErrorsFor (lastName,'El Apellido contiene menos de dos caracteres') ;
 
     }else{
         setSuccesFor(lastName)
@@ -107,13 +73,8 @@ function setSuccesFor(input){
 
 //Email
 
-<<<<<<< HEAD:public/validations/signup-scripts.js
-    if(email.value ==='') {
-        errors.push ('El Email está vacío');
-=======
     if(email.value == '') {
         setErrorsFor (email,'El Email está vacío');
->>>>>>> bruno-dev:src/validations/signup-scripts.js
     }
       var exp = /\w+@\w+\.+[a-z]/ ;                                //Aca son las expresiones regulares
     if(exp.test (email.value)) {
@@ -143,17 +104,10 @@ function setSuccesFor(input){
     errors.push('La contraseña debe contener al menos una letra en minuscula') ;
 
 }
-<<<<<<< HEAD:public/validations/signup-scripts.js
-    if (!password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)){                                                          //Expresion regular para caracteres
-    errors.push ('La contraseña debe poseer al menos un caracter especial') ;
-}
-
-=======
 /* if (!password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)){                                                          //Expresion regular para caracteres
     errors.push ('La contraseña debe poseer al menos un caracter especial') ;
 } */
-    
->>>>>>> bruno-dev:src/validations/signup-scripts.js
+
 //Imagen
     var fileExt = /(.jpg|.png)$/i;
 
@@ -163,17 +117,10 @@ function setSuccesFor(input){
     if(fileImage.value ==='') {
         errors.push ('Este campo es obligatorio')
     }
-<<<<<<< HEAD:public/validations/signup-scripts.js
 
-    if(errors.length > 0) {
-
-        e.preventDefault() ;                               //En caso de que haya errores, rechazamos la peticion por dafult del Submit
-=======
-    
     /* if(errors.length > 0) { */
 
        /*  e.preventDefault() ;                                 //En caso de que haya errores, rechazamos la peticion por dafult del Submit
->>>>>>> bruno-dev:src/validations/signup-scripts.js
         let errorsList = document.querySelector ('.errors-signup')
         for (let i = 0 ; i < errors.length ;i++) {
             errorsList.innerHTML +='<li>' + errors[i] + '</li>' ;              //Recorremos el array de errores ppara hacer un listado de ellos en el ul de errors-signup
@@ -183,20 +130,13 @@ function setSuccesFor(input){
 
     })
 
-<<<<<<< HEAD:public/validations/signup-scripts.js
+
+
+
+
 })
 
-})
-=======
-
-    
- })
 
 
 
 
-
-
-
-
->>>>>>> bruno-dev:src/validations/signup-scripts.js
