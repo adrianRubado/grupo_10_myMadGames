51365b9866d85c584bb3f2c59739b6f6635bc0fe
BrunoCostales <costalesbruno@gmail.com>
2021-09-14 23:30:37 -0300

    Se implemento validacion en el signin (falta matcheo)

--- public/validations/signin-scripts.js
+++ public/validations/signin-scripts.js
@@ -6,83 +6,56 @@ window.addEventListener('load', function () {
 
     let formsSignIn = document.querySelector('#formularioSignIn');
 
-
-    //Definimos un array con los errores a reflejar
-
-
-    let errors = [];
-
-
-
+    //CONTADOR
+    let count =0;
 
     const email = document.querySelector('#email');
     const password = document.querySelector('#password');
+    const buttom =document.querySelector('botonSignUp')
+
+    formsSignIn.addEventListener('submit', async function (e){
+        e.preventDefault(); 
+
+        
+
+       
     
 
-                             
-    const small = document.querySelector('small');
-
-
-
-    formsSignIn.addEventListener('submit', function (e){
-        e.preventDefault();
-
-        control();
-    })
-
-        function control()   {
+       
             
             const emailValue = email.value.trim();
             const passwordValue = password.value.trim();
             
-           
     
-            function setErrorsFor(input, msg) {
-                const formular = input.parentElement;
-                const small = formular.querySelector('small');
                 
         
-                small.innerText = msg ;
         
-                
-                formular.classList.remove('success');
-                formular.classList.add ('error');
-            }
-        
-            function setSuccesFor(input) {
-                const formular = input.parentElement;
-                
-                formular.classList.remove('error');
-                formular.classList.add('success') ;
-
-        
-        
-            }
-
              if(emailValue===''){
-                 setErrorsFor (email, 'El email esta sin completar')
-
-             
-
-             }else{
-                 setSuccesFor(email) ;
+             'El email esta sin completar'
              }
-
-             
-
-
-
-
              if(passwordValue===''){
-                 setErrorsFor(password, 'La contraseña esta sin completar')
+                 'La contraseña es obligatoria'
              
-             }else {
-                 setSuccesFor(password)
              }
+           
+             try {
+                const data = {
+                    email: email.value
+                  }
+                const res = await axios.post('http://localhost:3002/user/sign-up/check', {data});
+                const emailrepeat = (res.data.error);
+
+                if (!emailrepeat){
+                    'Invalid credentials'
+                }
+                
+            } catch (error) {
+                console.log(error.message)
+                
+            }
         
         
         
-        
-        
-        
-        }})
+    
+        })
+})
