adc8bbb14426c2e4dee559cd0b709fbe7b6da458
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-09-16 05:54:22 -0300

    jsfront new pass

---
+++ public/js/new-pass.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", () => {      
+
+const submitBtton = document.querySelector("#submit-new-pass");
+
+const allInputsConteiner = document.querySelectorAll(".contenedor-inputs-new-pass");
+
+const contenedorFormularioSignIn = document.querySelector(".contenedorFormularioSignIn")
+
+const formNewPass = document.querySelector("#formNewPass");
+
+const main = document.querySelector("#mainSignIn")
+
+
+const msgEmail = document.querySelector(".sent-email")
+
+console.log("hpla")
+
+submitBtton.addEventListener("click", (e)=>  {   
+e.preventDefault();
+
+contenedorFormularioSignIn.style.display = 'none'
+
+msgEmail.style.display = "flex"
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} )
