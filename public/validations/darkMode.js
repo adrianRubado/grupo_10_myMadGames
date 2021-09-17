a25dc8d96ab73c67adf5305310a10e555b161d5b
Adrian Ariel Rubado <aarubado@baufest.com>
2021-09-17 17:18:58 -0300

    darkMode

--- public/validations/darkMode.js
+++ public/validations/darkMode.js
@@ -1,10 +1,16 @@
 const toggle = document.querySelector(".toggle");
 const articles = document.querySelectorAll('article')
 
+const header = document.querySelector('header')
+
+const search = document.getElementById('custom-search-input')
 
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
   document.body.classList.toggle("miBody");
+  header.classList.toggle('header-dark')
+  search.classList.toggle('header-dark')
+
   toggle.checked = true
 
   articles.forEach(a =>{
@@ -20,6 +26,7 @@ if (currentTheme == "dark") {
 toggle.addEventListener("click", function () {
   if (toggle.checked) {
     document.body.classList.add("miBody2");
+    header.classList.add('header-dark')
 
     articles.forEach(a =>{
         a.classList.toggle('miBody2')
@@ -30,6 +37,7 @@ toggle.addEventListener("click", function () {
   } else {
     document.body.classList.remove("miBody");
     document.body.classList.remove("miBody2");
+    header.classList.remove('header-dark')
     var theme = 'light'
 
   }
