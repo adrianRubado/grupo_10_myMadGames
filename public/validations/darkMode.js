286582e8a285b7082a336b7b018fa02b388d6ff4
Adrian Ariel Rubado <aarubado@baufest.com>
2021-09-17 02:44:21 -0300

    dark mode index

--- public/validations/darkMode.js
+++ public/validations/darkMode.js
@@ -1,16 +1,52 @@
 window.addEventListener('load', async function (e) {
 
-    const toggle = document.querySelectorAll('.toggle')
 
-    toggle.forEach(t => {
-        console.log('holiiii')
-        t.addEventListener('click',function(e){
-       const dark = document.body
-       dark.classList.toggle('miBody')
-       const moreDark = document.body.article
+
+
+    const toggle = document.querySelector('.toggle')
+    const articles = document.querySelectorAll('article')
+
+    document.body.classList.toggle(localStorage.getItem('theme'))
+    articles.forEach(a =>{
+        a.classList.toggle(localStorage.getItem('theme'))
+    })
+
+
+
+        toggle.checked = this.localStorage.getItem('checked')
+        toggle.addEventListener('click',function(e){
+
+
+
+       if(toggle.checked){
+        const dark = document.body
+        dark.classList.toggle('miBody')
+        articles.forEach(a =>{
+            a.classList.toggle('miBody')
+        })
+        localStorage.setItem('theme','miBody')
+        localStorage.setItem('checked',toggle.checked)
+       }
+
+       else
+       if(toggle.checked == false){
+        localStorage.setItem('theme','naranja')
+        localStorage.setItem('checked',toggle.checked)
+        const dark = document.body
+        dark.classList.toggle('miBody')
+        articles.forEach(a =>{
+            a.classList.toggle('miBody')
+        })
+       }
+
+
+
+
 
     })
-})
+
+
+
 
 
 
