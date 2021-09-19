1cbd073dd1b73b843c61e6523893ce6ec3d00813
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-09-19 06:32:08 -0300

    edit controller - css - html

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -39,9 +39,15 @@ router.get("/search/" , productsController.search)
 
 
 router.get('/:id/',productsController.detail )
+
+
+
 router.get('/:id/edit',[authMiddleware,adminMiddleware], productsController.edit)
-router.put('/:id/edit',upload.single("image"), [authMiddleware,adminMiddleware] ,productsController.update)
-router.post('/favorites',authMiddleware,productsController.addFavorite)
+
+router.put('/:id/edit',upload.single("image"), /*[authMiddleware,adminMiddleware],*/ productsController.update);
+
+
+router.post('/favorites',authMiddleware,productsController.addFavorite);
 router.post('/',upload.single('image'),[
   check('name').not().isEmpty().withMessage ('Debes completar el Nombre del juego'),
   check('name').isLength({min :5}).withMessage ('El nombre debe contener minimo 5 caracteres'),
