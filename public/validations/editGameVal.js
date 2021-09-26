277a16c39eec43a357ef420ae78687d780f1c6d0
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-09-26 19:27:24 -0300

    radio buttons checked

--- public/validations/editGameVal.js
+++ public/validations/editGameVal.js
@@ -9,62 +9,40 @@ window.addEventListener('load', function(){
    const link = document.querySelector('#video-link-edit');
    const image = document.querySelector('#input-image-edit');
    const genre = document.querySelector('#genre-edit');
-
-
-
-    let formUpdate = document.querySelector('#editGameForm') ;
+   const  formUpdate = document.querySelector('#editGameForm') ;
     
     
     let errors = [] ;
-    formUpdate.addEventListener ('submit', async function(e){
-    
+    formUpdate.addEventListener ('submit',  function(e) {
+         
+        e.preventDefault();
      
-    
-        //NOMBRE
-        if(name.value ==''){
-            errors.push ('El nombre es un campo obligatorio')
-        }
-        if(name.value.length < 4){
-            errors.push ('El nombre debe contener al menos 5 caracteres')
-        }   
-    //DESCRIPCION
+        if(name.value == ''){
+            errors.push ('El juego debe tener un nombre')}   
         if(description.value.length < 20) {
-            errors.push ('La descripcion debe poseer al menos 20 caracteres') ;
-        }
+            errors.push ('La descripcion debe poseer al menos 20 caracteres') }
     
-    //IMAGEN
-        var fileExt = /(.jpg|.png|.jpeg|.gif|)$/i;
-        if (!fileExt.exec (image.value)) {
-            errors.push ('El tipo de archivo debe ser JPG / PNG / JPEG / GIF') ;
-        }
-    //PLATAFORMA
         if(platforms.value == undefined){
-            errors.push ('Debe pertenecer a almenos una plataforma')}
-    //LINK
+            errors.push ('El juego debe tener una plataforma asociada')}
+
         if(link.value ==''){
-                errors.push ('El LINK es un campo obligatorio')}
-    //GENERO
+                errors.push ('Te falto añadir una URL de Video')}
+    
         if(genre.value == undefined){
-                    errors.push ('Debe pertenecer a almenos un genero')}
-    //PRECIO
+                    errors.push ('El juego debe tener un género')}
+    
         if(price.value ==''){
-                        errors.push ('El precio es un campo obligatorio')}
-                       
+                        errors.push('El juego debe tener un precio')}
     
-    
-        
-    
-    
-             if(errors.length > 0) {
-        
-                            e.preventDefault() ;                                                   //En caso de que haya errores, rechazamos la peticion por dafult del Submit
-                            let errorsList = document.querySelector ('.errors-createGame')
+             if(errors.length > 0) {                                                  
+                            const errorUl = document.querySelector(".errors-js")
+                            const errorConteiner = document.querySelectorAll(".errors-container")
+                            errorConteiner.style.displayFlex = "column"
+                      
+
                             for (let i = 0 ; i < errors.length ;i++) {                                
-                                errorsList.innerHTML +='<li>' + errors[i] + '</li>' ;              //Recorremos el array de errores ppara hacer un listado de ellos en el ul de errors-signup
-                                
-                            }
-                            errorsList.style.color ('red') ;  //COLOR ROJO A LOS ERRORES
-    }
+                                errorsList.innerHTML +='<li>' + errors[i] + '</li>' ; } ;  
+    }else{e.submit()}
     
          
     })
