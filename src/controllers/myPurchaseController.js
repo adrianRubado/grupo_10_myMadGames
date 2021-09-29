170079b9858191779dcd9ae4758ae1214cfa0314
BrunoCostales <costalesbruno@gmail.com>
2021-09-29 20:16:02 -0300

    Agregando Mis compras Logica

--- src/controllers/myPurchaseController.js
+++ src/controllers/myPurchaseController.js
@@ -1,50 +1,69 @@
 const fs = require('fs');
 const path = require('path');
 const db = require("../../database/models");
+const game = require('../../database/models/game');
 const Op = db.Sequelize.Op;
 
 const   myPurchaseController ={
     view :  async(req,res)=> {
 
         const purchases =await db.User.findOne({
-            include:[{association :'userCart'}],
+           
             where: {
                 id :req.session.user.id
             }
             
         })
-        console.log(purchases);
-     /*    const games=[];
-        for (let i = 0; i < purchases.length; i++) {
-            for (let index = 0; index < purchases[i]; index++) {
-                const element = array[index];
-                
-            }
-            const game =
-             await db.Game.findOne ({
+        if (purchases) {
+            console.log(purchases);
+            let games=[];
+            for (let i = 0; i < purchases.length; i++) {
+                let products = await db.Product.findAll({
+                    where:{
+                        PurchaseId : purchases[i].id
+                    }
+                }) 
+                for (let index = 0; index < products.length; index++) {
+                    let game = {
 
-
-            })
+                        game:await db.Game.finOne ({
+                            where : {
+                                id : products[index].GameId
+                            }
+                        }),
+                        quantity: products[index].quantity,
+                        price:products[index].price,
+                        total:products[index].quantity * products[index].price
+                    }
+                    
+                    games.push (game)
+                    
+                }
             
         }
+        const viewdata = {
+            games:games
+            
+        }
+    
+    
+    
+    }else{
+        const viewdata ={
+            games:false
+        }
 
-
-
-
-           const viewdata = {
+        }
+        const viewdata = {
             game:game
         
-        } */
-        
-        
+        }
+        console.log(viewdata);
+        res.render ('myPurchases',viewdata);
+      
+     
+            }
+        }
+ 
 
-
-
-        res.render ('myPurchases');
-        } 
-        
-    
-
-
-}
 module.exports= myPurchaseController ;
