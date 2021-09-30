15e13f6c7725cee1a2f3c2a92fcdf0d548e87186
BrunoCostales <costalesbruno@gmail.com>
2021-09-30 20:58:03 -0300

    Implementacion correcta de la api

--- src/api/apiController.js
+++ src/api/apiController.js
@@ -7,21 +7,72 @@ const { where } = require('sequelize');
 const apiController ={
     users :async(req,res) =>{
        const users = await db.User.findAll()
-        const count = await users.length
-       
-       
-       const usersName = await users.map (usuario => usuario.first_name);
-       
-       console.log(usersName);
         
-        const data = {count,users}
+        
+       
+       
+        const datos = users.map(usuario => [id,first_name,email,detail] = [usuario.id, usuario.first_name, usuario.email,`http://localhost:3002/api/users/${usuario.id}`])
+        let array = []
+        let count = users.length
+
+        datos.forEach(element => {
+
+            let usuario = {
+                id : element[0],
+                name :element [1],
+                email :element [2],
+                detail :element [3]
+            }
+
+            array.push (usuario)
+        });
+
+
+
+        let data ={
+            count : count,
+            users:array
+        } 
+       
+        console.log(data);
+
+        
+        
+        
+
+
+        
+
+
+
+           
+      
+       
+    
+        
+        
        
        
        return res.json (data)
 
     },
     user : async(req,res) => {
-        const user = await db.User.findByPk(req.params.id)
+        const user = await db.User.findOne({
+            where :{
+                id :req.params.id
+            },
+        attributes: {
+            exclude: ['password', 'verify', 'RoleId','bday']
+        }
+    });
+        
+           
+       
+        
+        
+       
+       
+         
         
         return res.json (user);
 
--- src/api/apiProductController.js
+++ src/api/apiProductController.js
@@ -5,13 +5,42 @@ const apiController = require('./apiController');
 const apiProductController = {
     games: async (req,res) =>{
         const games = await db.Game.findAll ()
-        res.json (games)
-    }, 
+        const datos = games.map(juego => [id,naame,image,description,detail] = [juego.id,juego.name, juego.image, juego.description,`http://localhost:3002/api/products/${juego.id}`])
+    let array = []
+    let count = games.length
+    datos.forEach(element => {
+
+        let game = {
+            Id : element[0],
+            Name :element [1],
+            Image :element [2],
+            Description :element [3],
+            Detail :element[4]
+        }
+
+        array.push (game)
+    });
+    let data ={
+        count : count,
+        games:array
+    } 
+
+
+
+        res.json (data)
+    }
+    , 
     game:async(req,res )=>{ 
-        const game = await db.Game.findByPk (req.params.id)
-        res.json (game)
+        const game = await db.Game.findOne ({
+            where :{
+                id :req.params.id
+            },
+        attributes: {
+            exclude: [ 'platformId']
+        }
+    })
+        res.json (game)    
 
     }
-
 }
 module.exports = apiProductController
