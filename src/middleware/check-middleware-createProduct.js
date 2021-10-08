5460181df8769ee739450b614a12887238334719
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-10-08 16:53:24 -0300

    to merge

--- src/middleware/check-middleware-createProduct.js
+++ src/middleware/check-middleware-createProduct.js
@@ -3,11 +3,11 @@ const {check} = require("express-validator");
 
 
 module.exports = [
-    check('name').not().isEmpty().withMessage ('Debes completar el Nombre del juego'),
-    check('name').isLength({min :5}).withMessage ('El nombre debe contener minimo 5 caracteres'),
-  
-    check('description').isLength({min :20}).withMessage ('El nombre debe contener minimo 5 caracteres'),
-  
+    check('name').notEmpty().withMessage('Debes completar el Nombre del juego').bail(),
+    check('price').notEmpty().withMessage ('El nombre debe contener minimo 5 caracteres').bail(),
+     check('description').isLength({min :20}).withMessage ('El nombre debe contener minimo 5 caracteres').bail(),
+     check('genre').notEmpty().withMessage("Ingresa un género por favor").bail(),
+     check('platform').notEmpty().withMessage("Inresa una plataforma válida por favor").bail()
   
   
   ]
