98a0c4514d6a0dc6a79983742f6479dc657a6576
Adrian Ariel Rubado <aarubado@baufest.com>
2021-10-13 00:24:34 -0300

    mp buyDirect css

--- src/controllers/mercadoPagoController.js
+++ src/controllers/mercadoPagoController.js
@@ -5,43 +5,48 @@ const mpController = {
 
     success: async(req,res) => {
 
-
-
-        const purchase = await db.Purchase.create({
-            UserId : req.session.user.id,
-            status : 'Llega el lunes',
-            total : req.session.total
-        })
-
-        const products = req.session.purchaseDetail
-        for(let i = 0;i<products.length;i++){
-            await db.Product.create({
-                PurchaseId : purchase.id,
-                GameId : products[i].id,
-                quantity : products[i].quantity,
-                price : products[i].unit_price
-
+        try {
+            const purchase = await db.Purchase.create({
+                UserId : req.session.user.id,
+                status : 'Llega el lunes',
+                total : req.session.total
             })
-        }
 
-        delete req.session.total
-        delete req.session.purchaseDetail
+            const products = req.session.purchaseDetail
+            for(let i = 0;i<products.length;i++){
+                await db.Product.create({
+                    PurchaseId : purchase.id,
+                    GameId : products[i].id,
+                    quantity : products[i].quantity,
+                    price : products[i].unit_price
 
-
-
-
-        await db.Cart.destroy({
-            where :{
-                UserId : req.session.user.id
+                })
             }
-        })
 
-        const viewData = {
-            message : 'Gracias por su compra!!',
-            color : 'successs'
+            delete req.session.total
+            delete req.session.purchaseDetail
+
+
+
+
+            await db.Cart.destroy({
+                where :{
+                    UserId : req.session.user.id
+                }
+            })
+
+            const viewData = {
+                message : 'Gracias por su compra!!',
+                color : 'successs'
+
+            }
+            res.render('mercadopago',viewData)
+        } catch (error) {
+            console.log(error.message)
 
         }
-        res.render('mercadopago',viewData)
+
+
 },
     failure : (req,res) =>{
         const viewData = {
